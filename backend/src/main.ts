aa447703aa0bb1e07c6b4cc35770a74978140a05
sekkure <secreori@gmail.com>
2023-03-01 02:42:08 +0200

    feat(app): Added swagger and global validation pipe

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,25 @@
+import 'dotenv/config';
+import '@common/utils/check-envs.util';
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app/app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Sora API')
+    .setVersion('0.1')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, document);
+
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+
   await app.listen(3000);
 }
 bootstrap();
